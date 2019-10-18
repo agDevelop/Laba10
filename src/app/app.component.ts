a20145a775e32e4ac45dc4809577507e20a6a0d0
agDevelop <andrewnikolaevich.2014@gmail.com>
2019-10-18 15:01:11 +0300

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { isNullOrUndefined } from 'util';
+import { Sensor } from './shared/models/sensor.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proj181019';
+
+  sensors: Sensor[] = [];
+  sName = '';
+  sStatus = true;
+
+  constructor() {
+
+    for (let i = 0; i < 10; i++) {
+      this.sensors.push(new Sensor(i, `Датчик_${i}`));
+    }
+
+    console.log(this.sensors);
+  }
+
+  onDeleteSensor(id: number) {
+
+    this.sensors.splice(id, 1);
+  }
+
+  onAddSensor() {
+    this.sensors.push(new Sensor(this.sensors.length + 1, this.sName, this.sStatus));
+  }
 }
